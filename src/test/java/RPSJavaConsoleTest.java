c0a17f1175246cb62f2f7379408e311fb804ffa3
arivu-aata <arivu.aata@gmail.com>
2025-04-23 03:09:21 +0530

    Pilot failing test case

---
+++ src/test/java/RPSJavaConsoleTest.java
@@ -0,0 +1,36 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.io.PrintStream;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class RPSJavaConsoleTest {
+
+	@ParameterizedTest
+	@ValueSource(strings = { "ra", "a", "ble was" })
+	void invalidInputTerminatesPlay(String input) {
+		// Create a ByteArrayOutputStream to capture the output
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		PrintStream ps = new PrintStream(baos);
+
+		// Redirect standard output to the PrintStream
+		PrintStream oldOut = System.out;
+		System.setOut(ps);
+
+		// Set-up test stdin
+		InputStream oldIn = System.in;
+		System.setIn(new ByteArrayInputStream(input.getBytes()));
+
+		RPSJavaConsole.main(null);
+
+		System.setIn(oldIn);
+		System.setOut(oldOut);
+
+		assertEquals("Invalid Input! Terminating Play...", baos.toString());
+	}
+
+}

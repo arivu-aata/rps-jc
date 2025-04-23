19c1d2b2a656be320313283adbc468667f7c794b
arivu-aata <arivu.aata@gmail.com>
2025-04-23 14:00:18 +0530

    Making pilot test case pass

--- src/main/java/RPSJavaConsole.java
+++ src/main/java/RPSJavaConsole.java
@@ -1,9 +1,33 @@
+import java.util.Scanner;
+import java.util.Set;
 
 public class RPSJavaConsole {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	private static Set<Character> validInput = Set.of('r', 'p', 's');
 
+	public static void main(String[] args) {
+		try(Scanner scanner = new Scanner(System.in)) {
+			String input = scanner.next();
+			
+			if (isInvalid(input)) {
+				System.out.print("Invalid Input! Terminating Play...");
+				return;
+			}
+		}
+		
+	}
+
+	private static boolean isInvalid(String input) {
+		if (input.length() > 1) {
+			return true;
+		}
+		
+		char charInput = input.charAt(0);
+		if (validInput.contains(charInput)) {
+			return false;
+		}
+		
+		return true;
 	}
 
 }

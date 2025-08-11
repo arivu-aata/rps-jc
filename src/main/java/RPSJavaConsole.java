8a2e1dda0649b39bb5121912a77067a1733743ef
arivu-aata <arivu.aata@gmail.com>
2025-08-11 21:54:56 +0530

    removed side-effect from determineWinner

--- src/main/java/RPSJavaConsole.java
+++ src/main/java/RPSJavaConsole.java
@@ -35,7 +35,15 @@ public class RPSJavaConsole {
 		char playerMove = input.charAt(0);
 		char aiMove = RPSAI.move();
 
-		String winner = determineWinner(playerMove, aiMove);
+		String winner = DRAW;
+		try {
+			winner = determineWinner(playerMove, aiMove);
+		} catch (IllegalArgumentException e) {
+			if (e.getMessage().startsWith(OUTPUT_TYPE.INVALID_AI_MOVE)) {
+				writeOutput(aiMove, OUTPUT_TYPE.INVALID_AI_MOVE);
+				winner = PLAYER;
+			}
+		}
 
 		writeOutput(winner, OUTPUT_TYPE.WINNER);
 	}
@@ -75,8 +83,7 @@ public class RPSJavaConsole {
 		case 's':
 			return playerMove == 'r' ? PLAYER : playerMove == 'p' ? AI : DRAW;
 		default:
-			writeOutput(aiMove, OUTPUT_TYPE.INVALID_AI_MOVE);
-			return PLAYER;
+			throw new IllegalArgumentException(OUTPUT_TYPE.INVALID_AI_MOVE + " : " + aiMove);
 		}
 	}
 

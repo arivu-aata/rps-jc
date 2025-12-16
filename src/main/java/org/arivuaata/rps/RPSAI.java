16bf1f9ccd5c328970143d820d36d25028a434f2
arivu-aata <arivu.aata@gmail.com>
2025-12-16 21:26:57 +0530

    refactored

--- src/main/java/org/arivuaata/rps/RPSAI.java
+++ src/main/java/org/arivuaata/rps/RPSAI.java
@@ -3,9 +3,7 @@ package org.arivuaata.rps;
 public class RPSAI {
 
 	public static char getMove() {
-		int randomMoveNumber = (int) Math.floor(RPSPlayer.validInput.size() * Math.random());
-
-		return RPSPlayer.validInput.stream().skip(randomMoveNumber).findFirst().get();
+		return RPSPlayer.validInput.stream().findAny().get();
 	}
 
 }

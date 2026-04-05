64896acb4192e3a52e367c45d9a2c062358dfbc7
arivu-aata <arivu.aata@gmail.com>
2026-04-05 12:57:34 +0530

    introduced anyMove method

--- src/main/java/org/arivuaata/rps/RPSAI.java
+++ src/main/java/org/arivuaata/rps/RPSAI.java
@@ -3,6 +3,10 @@ package org.arivuaata.rps;
 public class RPSAI {
 
 	public static char getMove() {
+		return anyMove();
+	}
+
+	private static char anyMove() {
 		return RPSPlayer.validInput.stream().findAny().get();
 	}
 
